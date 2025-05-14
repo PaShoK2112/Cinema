9d01f9942a1fd0212f64975073390c402168d630
Павло bc2024-6 <pashababiy000@gmail.com>
2025-05-14 14:29:45 +0300

    create moviecard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; import '../styles/MovieCard.css';
+
+const MovieCard = ({ movie }) => (
+<div className="movie-card">
+<img src={movie.poster} alt={movie.title} />
+<h3>{movie.title}</h3>
+<p>{movie.description}</p>
+<span>Жанр: {movie.genre}</span>
+<span>Дата сеансу: {movie.date}</span>
+<span>Час: {movie.time}</span>
+</div>
+);
+
+export default MovieCard;
