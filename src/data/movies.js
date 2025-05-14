1b6220398f6b4391bba9311362ac1b1d992e7112
Павло bc2024-6 <pashababiy000@gmail.com>
2025-05-14 15:04:01 +0300

    create movies.js

---
+++ src/data/movies.js
@@ -0,0 +1,36 @@
+const movies = [
+    {
+        id: 1,
+        title: "Людина-мураха",
+        description: "«Людина-мураха» - американський фільм Пейтона Ріда про супергероїв.", 
+        genre: "Екшн/Наукова фантастика",
+        time: "2025-05-02 20:00",
+        poster: "https://upload.wikimedia.org/wikipedia/uk/5/5c/%D0%9F%D0%BE%D1%81%D1%82%D0%B5%D1%80_%D1%84%D1%96%D0%BB%D1%8C%D0%BC%D1%83_%C2%ABAnt-Man%C2%BB.jpg",
+    },
+    {
+        id: 2,
+        title: "Чорна Пантера",
+        description: "«Чо́рна Панте́ра» — американський супергеройський фільм на основі однойменних коміксів видавництва Marvel Comics, виробництва Marvel Studios і дистрибуції Walt Disney Studios Motion Picture",
+        genre: "Екшн , Пригоди , Фантастика , 2018", 
+        time: "2025-05-02 21:30",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgmhzY0hy1-BuPBxPi725G2FHzWwFAyA9Qrw&s",
+    },
+    {
+        id: 3,
+        title: "П'ять ночей у Фредді",
+        description: "«П'ять ночей у Фредді» — американський надприродний фільм жахів, знятий Еммою Таммі, яка написала сценарій разом зі Скоттом Коутоном і Сетом Каддебеком, заснований на однойменній серії відеоігор, створених Коутоном.",
+        genre: "Жахи/Детектив", 
+        time: "2025-05-03 18:30",
+        poster: "https://upload.wikimedia.org/wikipedia/uk/d/d2/%D0%9F%27%D1%8F%D1%82%D1%8C_%D0%BD%D0%BE%D1%87%D0%B5%D0%B9_%D1%83_%D0%A4%D1%80%D0%B5%D0%B4%D0%B4%D1%96%2C_2023%2C_%D0%BF%D0%BE%D1%81%D1%82%D0%B5%D1%80.jpg",
+
+    },
+    {   id: 4,
+        title: "Круті стволиі",
+        description: "«Круті стволи» — бойовик 2016 року Джеймса Веткінса спільного виробництва Франції, Люксембургу і Сполучених Штатів.", 
+        genre: "Екшн/Трилер",
+        time: "2025-05-03 20:30",
+        poster: "https://eneyida.tv/uploads/posts/2020-02/1582482253_1.jpg",
+
+    },
+];
+    export default movies;
