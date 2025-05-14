b1c505f92d89d2602048a1313cb7fe0a95c9a54b
Павло bc2024-6 <pashababiy000@gmail.com>
2025-05-14 15:32:22 +0300

    add movielist.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import MovieList from './components/MovieList'; import movies from './data/movies';
+import MovieList from './components/MovieList'; 
+import movies from './data/movies';
+import './App.css'
 
 const App = () => { 
   return (
     <div>
-      <h1 style={{ textAlign: 'center', color: '#e0e8f3' }}>Список фільмів</h1>
+      <h1 >Список фільмів</h1>
       <MovieList movies={movies} />
     </div>
 );
