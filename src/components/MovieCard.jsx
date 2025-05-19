ee4d505a4ca769258c94cd6f1d498a36a70df088
Павло bc2024-6 <pashababiy000@gmail.com>
2025-05-19 22:52:01 +0300

    update file MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,19 @@
-import React from 'react'; 
-import '../styles/MovieCard.css';
+import React from 'react';
+import { Link } from 'react-router-dom'; import '../styles/MovieCard.css';
 
-const MovieCard = ({ movie }) => (
+const MovieCard = ({ movie }) => { return (
 <div className="movie-card">
 <img src={movie.poster} alt={movie.title} />
 <h3>{movie.title}</h3>
 <p>{movie.description}</p>
-<span>Жанр: {movie.genre}</span>
-<span>Дата сеансу: {movie.date}</span>
-<span>Час: {movie.time}</span>
+<p>Жанр: {movie.genre}</p>
+<p>Час сеансу: {movie.time}</p>
+
+<Link to={`/booking/${movie.id}`}>
+<button className="book-button">Забронювати</button>
+</Link>
 </div>
 );
+};
 
 export default MovieCard;
